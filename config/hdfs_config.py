d965dfaeaf36f631333256a5ceadde132beb120e
Anthony-Cormeaux <anthonycormeaux@gmail.com>
2024-11-18 23:22:47 +0100

    hadoop installation

--- config/hdfs_config.py
+++ config/hdfs_config.py
@@ -1,40 +1,15 @@
-
-# # HDFS configuration
-# HDFS_HOST = 'localhost'  # Replace with your HDFS host
-# HDFS_PORT = 9870         # Default WebHDFS port
-# HDFS_RAW_DEST_PATH = '/user/hadoop/data/raw'  # Destination path in HDFS
-# HDFS_CLEAN_DEST_PATH = '/user/hadoop/data/cleaned'  # Destination path in HDFS
-# LOCAL_RAW_DATA_PATH = '/home/freddy/Documents/Cours_efrei/Data_integration/Projets_data_integration/data_integration_student_loans/data/raw'
-
-# # Additional HDFS settings
-# REPLICATION_FACTOR = 1  # Default replication factor (adjust for cluster setup)
-
-
-# hdfs_config.py
-
 class HDFSConfig:
-    """
-    Configuration settings for HDFS integration in a data pipeline.
-    
-    Attributes:
-        HOST (str): The hostname or IP address of the HDFS Namenode.
-        PORT (int): The WebHDFS port for HTTP connections.
-        RAW_DEST_PATH (str): HDFS path for storing raw, unprocessed data.
-        CLEAN_DEST_PATH (str): HDFS path for storing cleaned or processed data.
-        LOCAL_RAW_DATA_PATH (str): Local path where raw data is initially stored.
-        REPLICATION_FACTOR (int): Default replication factor for HDFS files.
-    """
-    
     # HDFS Connection Settings
-    HOST = 'localhost'  # Replace with your HDFS host
-    PORT = 9870         # Default WebHDFS port
+    HOST = 'localhost'
+    PORT = 9870 # Make sure this matches the port you've set in core-site.xml
 
     # HDFS Destination Paths
-    RAW_DEST_PATH = '/user/hadoop/data/raw'      # Destination path in HDFS for raw data
-    CLEAN_DEST_PATH = '/user/hadoop/data/cleaned'  # Destination path in HDFS for cleaned data
+    RAW_DEST_PATH = '/user/anthonycormeaux/data/raw'
+    CLEAN_DEST_PATH = '/user/anthonycormeaux/data/cleaned'
 
     # Local Paths
-    LOCAL_RAW_DATA_PATH = '/home/freddy/Documents/Cours_efrei/Data_integration/Projets_data_integration/data_integration_student_loans/data/raw'
-    LOCAL_CLEAN_DATA_PATH = '/home/freddy/Documents/Cours_efrei/Data_integration/Projets_data_integration/data_integration_student_loans/data/cleaned'
+    LOCAL_RAW_DATA_PATH = '/Users/anthonycormeaux/Documents/Projet_data_integration/Nouvelle version/data_integration_student_loans/data/raw'
+    LOCAL_CLEAN_DATA_PATH = '/Users/anthonycormeaux/Documents/Projet_data_integration/Nouvelle version/data_integration_student_loans/data/cleaned'
+
     # HDFS File Settings
-    REPLICATION_FACTOR = 1  # Adjust for cluster setup
+    REPLICATION_FACTOR = 1
